d746c5ac77135591aabf7a2c51732677091cee3a
leetcat <leetcat@cs.ubc.ca>
2016-02-26 09:06:57 -0800

    Initial commit

---
+++ www/js/controllers.js
@@ -0,0 +1,66 @@
+/**
+ * Created by leetcat on 2/25/16.
+ */
+angular.module('map.controllers', [])
+
+    .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+
+      // With the new view caching in Ionic, Controllers are only called
+      // when they are recreated or on app start, instead of every page change.
+      // To listen for when this page is active (for example, to refresh data),
+      // listen for the $ionicView.enter event:
+      //$scope.$on('$ionicView.enter', function(e) {
+      //});
+
+      // Form data for the login modal
+      $scope.loginData = {};
+
+      // Create the login modal that we will use later
+      $ionicModal.fromTemplateUrl('templates/login.html', {
+        scope: $scope
+      }).then(function(modal) {
+        $scope.modal = modal;
+      });
+
+      // Triggered in the login modal to close it
+      $scope.closeLogin = function() {
+        $scope.modal.hide();
+      };
+
+      // Open the login modal
+      $scope.login = function() {
+        $scope.modal.show();
+      };
+
+      // Perform the login action when the user submits the login form
+      $scope.doLogin = function() {
+        console.log('Doing login', $scope.loginData);
+
+        // Simulate a login delay. Remove this and replace with your login
+        // code if using a login system
+        $timeout(function() {
+          $scope.closeLogin();
+        }, 1000);
+      };
+    })
+
+
+    .controller('MapCtrl', function($scope, $state, $cordovaGeolocation) {
+      var options = {timeout: 10000, enableHighAccuracy: true};
+
+      $cordovaGeolocation.getCurrentPosition(options).then(function(position){
+
+        var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+        var mapOptions = {
+          center: latLng,
+          zoom: 15,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+
+        $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+      }, function(error){
+        console.log("Could not get location");
+      });
+    });
